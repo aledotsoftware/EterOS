0172ac6d7e4f92ac11dfffaf8cb00c81cc6cdc5c
Alejandro Avellaneda <Aleavellaneda1@gmail.com>
2026-02-14 19:22:33 -0300

    Update defs.h

--- include/net/defs.h
+++ include/net/defs.h
@@ -1,7 +1,7 @@
 /**
  * éterOS - Definiciones de Red Genéricas
  *
- * Estructuras y constantes para protocolos de red básicos (Ethernet, IP, UDP).
+ * Estructuras y constantes para protocolos de red básicos (Ethernet, IP, UDP, TCP).
  */
 
 #ifndef ETEROS_NET_DEFS_H
@@ -10,8 +10,10 @@
 #include "../types.h"
 
 /* Network Constants */
-#define ETHERNET_TYPE_IP 0x0800
+#define ETHERNET_TYPE_IP  0x0800
+#define ETHERNET_TYPE_ARP 0x0806
 #define IP_PROTO_UDP 17
+#define IP_PROTO_TCP 6
 
 /* Byte Order Utils (x86 is Little Endian) */
 static inline uint16_t htons(uint16_t v) { return (v << 8) | (v >> 8); }
@@ -47,5 +49,34 @@ struct udp_header {
     uint16_t len;
     uint16_t checksum;
 } __attribute__((packed));
+
+struct arp_packet {
+    uint16_t htype;
+    uint16_t ptype;
+    uint8_t  hlen;
+    uint8_t  plen;
+    uint16_t op;
+    uint8_t  src_mac[6];
+    uint32_t src_ip;
+    uint8_t  dest_mac[6];
+    uint32_t dest_ip;
+} __attribute__((packed));
+
+struct tcp_header {
+    uint16_t src_port;
+    uint16_t dest_port;
+    uint32_t seq;
+    uint32_t ack_seq;
+    uint16_t flags; /* [4 bits offset][6 bits reserved][6 bits flags] */
+    uint16_t window;
+    uint16_t checksum;
+    uint16_t urgent_ptr;
+} __attribute__((packed));
+
+#define TCP_FIN (1 << 0)
+#define TCP_SYN (1 << 1)
+#define TCP_RST (1 << 2)
+#define TCP_PSH (1 << 3)
+#define TCP_ACK (1 << 4)
 
 #endif /* ETEROS_NET_DEFS_H */
