cd65a94dc540d8844d86f5610f77652e58e5d650
Alejandro Avellaneda <Aleavellaneda1@gmail.com>
2026-02-15 16:37:06 -0300

    Create wait.h

---
+++ userspace/libc/include/sys/wait.h
@@ -0,0 +1,16 @@
+#ifndef _SYS_WAIT_H
+#define _SYS_WAIT_H
+
+#include <stdint.h>
+
+typedef int pid_t;
+
+pid_t waitpid(pid_t pid, int *status, int options);
+
+/* Macros for interpreting status */
+#define WIFEXITED(s)    (((s) & 0x7f) == 0)
+#define WEXITSTATUS(s)  (((s) & 0xff00) >> 8)
+#define WIFSIGNALED(s)  (((s) & 0x7f) > 0 && ((s) & 0x7f) < 0x7f)
+#define WTERMSIG(s)     ((s) & 0x7f)
+
+#endif /* _SYS_WAIT_H */
