208df98e10fd21e55a82cec7d32b0ee5008d0572
Alejandro Avellaneda <Aleavellaneda1@gmail.com>
2026-02-15 03:45:22 -0300

    Create lock.h

---
+++ include/lock.h
@@ -0,0 +1,24 @@
+#ifndef ETEROS_LOCK_H
+#define ETEROS_LOCK_H
+
+#include <types.h>
+
+typedef volatile int spinlock_t;
+
+static inline void spin_lock(spinlock_t *lock) {
+    while (__sync_lock_test_and_set(lock, 1)) {
+        while (*lock) {
+            __asm__ volatile("pause");
+        }
+    }
+}
+
+static inline void spin_unlock(spinlock_t *lock) {
+    __sync_lock_release(lock);
+}
+
+static inline int spin_trylock(spinlock_t *lock) {
+    return !__sync_lock_test_and_set(lock, 1);
+}
+
+#endif /* ETEROS_LOCK_H */
