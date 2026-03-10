62039d14762db8d011e37a53c26812a179185433
Alejandro Avellaneda <Aleavellaneda1@gmail.com>
2026-03-10 10:32:25 -0300

    a

--- fix_repo.sh
+++
@@ -1,1 +0,0 @@
-git checkout origin/main -- userspace/login.c userspace/passwd.c userspace/useradd.c userspace/userdel.c userspace/sh.c userspace/test_security.c
--- test_sig.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-grep -n "sys_rt_sigaction" kernel/arch/x86_64/syscall.c
-grep -n "sys_rt_sigreturn" kernel/arch/x86_64/syscall.c
